01b183a15b83567fa12cee021c8d5ffd77017ca8
Bridget Mills <bridgetsmills@gmail.com>
2019-09-16 16:55:46 -0500

    java syntax exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,65 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 19;
+        String myString;
+//        myString="Bridget";
+        //prints out string
+//        myString = 'n';
+        //error incompatible type when assigning a char to a string.
+//        myString= 3.14159;
+        //error double cannot be converted to string
+//        System.out.println(myString);
+
+//        long myNumber;
+        //error myNumber was not initialized compilation error
+//        myNumber = 3.14;
+        //error lossy conversion from double long
+        //long can not have fractional parts
+//        myNumber = 123L;
+       // prints number
+//        myNumber = 123;
+        //prints out number 123
+//        float myNumber;
+//        myNumber =3.14;
+        //error lossy conversion from double to float
+        //2 ways to fix
+//        float myNumber = (int) 3.14;
+//   //assumes the number is a double so add an F to the end of the number  or explicit casting
+//        long morePrecise = myInteger;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        //takes in the value of x first and outputs it...which is 5 then increments it so that the next time it is called it is now 6
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        //increments the number the variable by one then give the output...then displays the new output
+//        String class = "12";
+        //all kinds of errors
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        //I think it will give an error, because even with explicit casting I don't think it will change the string to an int
+        //matched my expectations. There was a runtime error.
+//        int three = (int) "three";
+        //this give a compile error
+//        System.out.println(three);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+//        What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+        //the variable may or may not working depending on the type of number being assigned. it may be necessary to make an explicit assignment to the variable in order to assign it to a smaller type.
+
+    }
+}
